d83368584f03627894e472af638a49365e1c57bc
andyscoffee <67533002+andyscoffee@users.noreply.github.com>
2022-03-08 23:52:54 +0900

    Update 그리디_큰수만들기(오류).py
    테스트케이스 찾은거 저만큼 다 맞았는데도 틀렸다고나옴

--- 프로그래머스/코딩테스트입문/그리디_큰수만들기(오류).py
+++ 프로그래머스/코딩테스트입문/그리디_큰수만들기(오류).py
@@ -2,8 +2,11 @@ def solution(number, k):
     num = list(number)
     tmp = 0
     i = 0
-    
+    check = len(num)
+    check2 = k
     while k > 0:
+        if i+1 == len(num):
+            break
         if tmp == 0:
             tmp = num[i]
             continue
@@ -16,9 +19,30 @@ def solution(number, k):
         else:
             tmp = num[i+1]
         i += 1
-        
+    
+    if len(num)>check-check2:
+        for i in range(k):
+            del num[-1]
+    
     return ''.join(num)
 
 print(solution("1924", 2))
 print(solution("1231234", 3))
 print(solution("4177252841", 4))
+
+print(solution("87654321", 3))
+print(solution("18765432", 3))
+print(solution("77413258", 2))
+print(solution("12345678901234567890", 19))
+print(solution("01010", 3))
+print(solution("559913", 1))
+print(solution("9191919", 1))
+"""
+"87654321", 3, "87654"
+"18765432", 3, "87654"
+"77413258", 2, "774358"
+"12345678901234567890", 19, "9"
+"01010", 3, "11"
+"559913", 1, "59913"
+"9191919", 1, "991919"
+"""
