776536674ee57f46f37e7d639037070859bee014
andyscoffee <monarch071@gmail.com>
2022-04-11 23:13:18 +0900

    BOJ 3053

---
+++ 백준/기본 수학 2/택시 기하학.py
@@ -0,0 +1,11 @@
+# 3053
+from math import pi
+"""
+택시 기하학에서 두 점 T1(x1,y1), T2(x2,y2) 사이의 거리는 다음과 같이 구할 수 있다.
+
+D(T1,T2) = |x1-x2| + |y1-y2|
+원: 평면 상의 어떤 점에서 거리가 일정한 점들의 집합
+"""
+r = int(input())
+print(pi*pow(r, 2))
+print(pow(r, 2)*2)
