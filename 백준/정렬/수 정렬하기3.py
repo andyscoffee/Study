06a4337319a6c3aabb2763ec5175659069273f1f
andyscoffee <monarch071@gmail.com>
2022-04-15 00:17:01 +0900

    BOJ 10989

---
+++ 백준/정렬/수 정렬하기3.py
@@ -0,0 +1,12 @@
+# 10989 메모리 제한 8mb
+
+import sys
+
+n = int(sys.stdin.readline())
+data = [0]*10001
+for _ in range(n):
+    data[int(sys.stdin.readline())] += 1
+for i in range(10001):
+    if data != 0:
+        for j in range(data[i]):
+            print(i)
