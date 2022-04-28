9a6b07d0f17b5b7cf28b2b21ba243f3fee5f8472
andyscoffee <monarch071@gmail.com>
2022-04-28 23:23:14 +0900

    BOJ 10866

---
+++ 백준/큐, 덱/덱.py
@@ -0,0 +1,46 @@
+# 10866
+
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+data = deque()
+for _ in range(n):
+    wtd = sys.stdin.readline().split()
+    if len(wtd) > 1:
+        if wtd[0] == "push_front":
+            data.appendleft(wtd[1])
+        else:
+            data.append(wtd[1])
+
+    elif wtd[0] == "pop_front":
+        if data:
+            print(data.popleft())
+        else:
+            print(-1)
+    elif wtd[0] == "pop_back":
+        if data:
+            print(data.pop())
+        else:
+            print(-1)
+
+    elif wtd[0] == "size":
+        print(len(data))
+
+    elif wtd[0] == "empty":
+        if len(data) > 0:
+            print(0)
+        else:
+            print(1)
+
+    elif wtd[0] == "front":
+        if data:
+            print(data[0])
+        else:
+            print(-1)
+
+    elif wtd[0] == "back":
+        if data:
+            print(data[-1])
+        else:
+            print(-1)
