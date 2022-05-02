9939870cd193c76cc945c767cf8981c696c0d7c0
andyscoffee <monarch071@gmail.com>
2022-05-03 01:08:08 +0900

    BOJ 14889

--- 백준/백트래킹/스타트와 링크.py
+++ 백준/백트래킹/스타트와 링크.py
@@ -3,17 +3,24 @@ from itertools import combinations
 import sys
 
 n = int(sys.stdin.readline().strip())
-player = [i for i in range(1, n+1)]
-print(player)
-data = []
-min_sub = 1e9
+players = [i for i in range(n)]
+data = [[0] for _ in range(n)]
+ans = 1e9
 
 for i in range(n):
-    data.append(list(map(int, sys.stdin.readline().split())))
+    data[i] = list(map(int, sys.stdin.readline().split()))
 
-tmp = list(combinations(player, n//2))
-for t in tmp:
-    teamA = list(t)
-    teamB = [x for x in data if x not in t]
-    print("A: ", teamA)
-    print("B: ", teamB)
+team = list(combinations(players, n//2))
+for t in team:
+    teamA = 0
+    teamB = 0
+    for i in t:
+        for j in t:
+            teamA += data[i][j]
+    team_2 = [i for i in range(n)if i not in t]
+    for i in team_2:
+        for j in team_2:
+            teamB += data[i][j]
+    if ans > abs(teamA-teamB):
+        ans = abs(teamA-teamB)
+print(ans)
