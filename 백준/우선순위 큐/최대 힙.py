a3e7a7e8dd5d239191a9b5e03af4df12de4fd343
andyscoffee <monarch071@gmail.com>
2022-05-17 22:52:01 +0900

    BOJ 11279

---
+++ 백준/우선순위 큐/최대 힙.py
@@ -0,0 +1,16 @@
+# 11279
+
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+h = []
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if h:
+            print(-heapq.heappop(h))
+        else:
+            print(0)
+    else:
+        heapq.heappush(h, -x)
