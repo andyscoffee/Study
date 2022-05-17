72e05768e4027ef7163e43fa5fb0d803680937ee
andyscoffee <monarch071@gmail.com>
2022-05-17 22:52:10 +0900

    BOJ 11286

---
+++ 백준/우선순위 큐/절대값 힙.py
@@ -0,0 +1,16 @@
+# 11286
+
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+h = []
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    if x == 0:
+        if h:
+            print(heapq.heappop(h)[1])
+        else:
+            print(0)
+    else:
+        heapq.heappush(h, (abs(x), x))
