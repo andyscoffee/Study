b8d729e9f25c7fb984d7928258c7df14292d8233
andyscoffee <monarch071@gmail.com>
2022-06-08 23:24:36 +0900

    Day 3, 오류 수정

--- Pytorch/데이터 분석/DataClassifier.py
+++ Pytorch/데이터 분석/DataClassifier.py
@@ -6,6 +6,10 @@ import torch.nn.functional as F
 import numpy as np
 import torch.optim as optim
 from torch.optim import Adam
+# ONNX관련 새로 임포트한 것들
+import io
+import torch.utils.model_zoo as model_zoo
+import torch.onnx
 
 # 붓꽃(Iris) 데이터 불러오기
 df = pd.read_excel(
@@ -242,6 +246,36 @@ def test_species():
         print('Accuracy to predict %5s : %2d %%' %
               (label_list[i], 100 * labels_correct[i] / labels_total[i]))
 
+# Day 3
+# 기계 학습 모델(.pth파일)을 Windows ML앱과 통합하기 위해 모델을 ONNX 형식으로 변환하기
+
+# ONNX 형식으로 변환하기 위한 함수
+
+
+# Function to Convert to ONNX
+def convert():
+
+    # set the model to inference mode
+    model.eval()
+
+    # Let's create a dummy input tensor
+    dummy_input = torch.randn(1, 3, 32, 32, requires_grad=True)
+
+    # Export the model
+    torch.onnx.export(model,         # model being run
+                      # model input (or a tuple for multiple inputs)
+                      dummy_input,
+                      "Network.onnx",       # where to save the model
+                      export_params=True,  # store the trained parameter weights inside the model file
+                      opset_version=11,    # the ONNX version to export the model to
+                      do_constant_folding=True,  # whether to execute constant folding for optimization
+                      input_names=['input'],   # the model's input names
+                      output_names=['output'],  # the model's output names
+                      dynamic_axes={'input': {0: 'batch_size'},    # variable length axes
+                                    'output': {0: 'batch_size'}})
+    print(" ")
+    print('Model has been converted to ONNX')
+
 
 # 메인 함수(모델 학습 시작-> 모델 저장 ->결과 표시)
 if __name__ == "__main__":
@@ -250,3 +284,4 @@ if __name__ == "__main__":
     print('Finished Training\n')
     test()
     test_species()
+    convert()
