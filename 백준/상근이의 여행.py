fb38cdc324bc95578a152326195bbfff69791a35
andyscoffee <monarch071@gmail.com>
2022-06-10 23:44:24 +0900

    BOJ 9372

--- 백준/상근이의 여행.py
+++ 백준/상근이의 여행.py
@@ -1,33 +1,10 @@
-def fp(parent, x):
-    if parent[x] != x:
-        parent[x] = fp(parent, parent[x])
-    return parent[x]
+# 9372
 
+import sys
 
-def up(parent, a, b):
-    a = fp(parent, a)
-    b = fp(parent, b)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
-
-
-tc = int(input())
-for t in range(tc):
-    n, m = map(int, input().split())
-    parent = [0]*(n+1)
-    edges = []
-    res = 0
-    for i in range(1, n+1):
-        parent[i] = i
-
+tc = int(sys.stdin.readline())
+for _ in range(tc):
+    n, m = map(int, sys.stdin.readline().split())
     for _ in range(m):
-        a, b = map(int, input().split())
-        edges.append((1, a, b))
-    edges.sort()
-    for edge in edges:
-        cost, a, b = edge
-        if fp(parent, a) != fp(parent, b):
-            up(parent, a, b)
-            res += cost
+        a, b = map(int, sys.stdin.readline().split())
+    print(n-1)  # MST로 모두 연결되어 있다면 간선의 개수는 n-1개
