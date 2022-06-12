fc288420787cea8a6a51c83bd23e8758f384f2bf
andyscoffee <monarch071@gmail.com>
2022-06-12 23:56:18 +0900

    BOJ 1021

--- 백준/큐, 덱/회전하는 큐.py
+++ 백준/큐, 덱/회전하는 큐.py
@@ -3,6 +3,23 @@
 import sys
 from collections import deque
 
-n, m = int(sys.stdin.readline().split())
+n, m = map(int, sys.stdin.readline().split())
 needs = list(map(int, sys.stdin.readline().split()))
 data = deque([i for i in range(1, n+1)])
+cnt = 0
+
+for i in needs:
+    while True:
+        if data[0] == i:
+            data.popleft()
+            break
+        else:
+            if data.index(i) < len(data)/2:
+                while data[0] != i:
+                    data.append(data.popleft())
+                    cnt += 1
+            else:
+                while data[0] != i:
+                    data.appendleft(data.pop())
+                    cnt += 1
+print(cnt)
