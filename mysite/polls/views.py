eb6ef211184e32b263cb90d1d03592ef1d8ff2c2
andyscoffee <monarch071@gmail.com>
2022-06-27 23:50:39 +0900

    Writing your first Django app, part 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 
 
 def index(request):
     # pub_date를 최신순으로 정렬하여 latest_qustion_list로 넘겨줌
     latest_question_list = Question.objects.order_by('-pub_data')[:5]
     # join 함수를 통해 output 문자열로 합침
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
     # http리퀘스트가 오면 response로 위의 메세지 반환
 
 
