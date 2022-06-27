7973a43a17e6b2dd6b8cdf379857c7294d3bfe80
andyscoffee <monarch071@gmail.com>
2022-06-27 23:50:10 +0900

    BOJ 10968

--- 백준/누적 합/나머지 합.py
+++ 백준/누적 합/나머지 합.py
@@ -4,13 +4,16 @@ import sys
 
 n, m = map(int, sys.stdin.readline().split())
 data = list(map(int, sys.stdin.readline().split()))
-tmp, end = 0, 0
-cnt = 0
-for start in range(n):
-    while tmp < m and end < n:
-        tmp += data[end]
-        end += 1
-    if tmp % m == 0:
-        cnt += 1
-    tmp -= data[start]
-print(cnt)
+
+prepix = [0]*m
+p_sum, ans = 0, 0
+prepix[0] = 1
+
+for i in range(n):
+    p_sum += data[i]  # 부분합 저장
+    prepix[p_sum % m] += 1  # 나머지가 같은 두 부분합 구간 -> m의 배수
+
+for i in prepix:
+    ans += i*(i-1)//2  # 나머지가 0인 경우, 부분합 자체가 m의 배수기에 index가 0인 것을 포함
+
+print(ans)
