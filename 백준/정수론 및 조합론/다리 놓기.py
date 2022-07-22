0bd88cf6e1f8c1ee233708e8832ba714a74e1518
andyscoffee <monarch071@gmail.com>
2022-07-22 23:51:28 +0900

    BOJ 1010

--- 백준/정수론 및 조합론/다리 놓기.py
+++ 백준/정수론 및 조합론/다리 놓기.py
@@ -1,10 +1,21 @@
 # 1010
 
 import sys
+
+
+def fac(x):
+    res = 1
+    for i in range(1, x+1):
+        res *= i
+    return res
+
+
 tc = int(sys.stdin.readline())
 for _ in range(tc):
     n, m = map(int, sys.stdin.readline().split())
     if n == m:
         print(1)
-    else:
-        print()
+        continue
+    else:  # m개의 지역에 n개의 다리 -> mCn = m!/(m-n)!n!
+        ans = fac(m)//(fac((m-n))*fac(n))
+        print(ans)
