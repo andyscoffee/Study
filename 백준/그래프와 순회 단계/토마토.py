b559b82cd99fa10ea3b9af52f0dede692ffca2bd
andyscoffee <monarch071@gmail.com>
2022-10-19 00:44:02 +0900

    BOJ 75576

--- 백준/그래프와 순회 단계/토마토.py
+++ 백준/그래프와 순회 단계/토마토.py
@@ -3,30 +3,38 @@
 import sys
 from collections import deque
 
+
+def bfs():
+    while q:
+        a, b = q.popleft()
+        for i in range(4):
+            nx = a + dx[i]
+            ny = b + dy[i]
+            if 0 <= nx < N and 0 <= ny < M:
+                if graph[nx][ny] == 0:  # 익지 않은 토마토일 경우만
+                    graph[nx][ny] = graph[a][b] + 1
+                    q.append((nx, ny))
+
+
 M, N = map(int, sys.stdin.readline().split())
 graph = []
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
+q = deque()
+ans = 0
 for _ in range(N):
     graph.append(list(map(int, sys.stdin.readline().split())))
-
-
-def bfs(x, y):
-    q = deque()
-    q.append((x, y))
-    while q:
-        a, b = q.popleft()
-        for i in range(4):
-            nx = a+dx[i]
-            ny = b + dy[i]
-            if nx <= 0 or ny <= 0 or nx > N or ny > M:
-                if graph[nx][ny] == 0:  # 벽인 경우
-                    graph[nx][ny] = graph[a][b]+1
-                    q.append((nx, ny))
-    return graph[N-1][M-1]
-
 
 for i in range(N):
     for j in range(M):
         if graph[i][j] == 1:
-            print()
+            q.append((i, j))
+
+bfs()
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 0:
+            print(-1)
+            exit(0)
+    ans = max(ans, max(graph[i]))
+print(ans-1)
