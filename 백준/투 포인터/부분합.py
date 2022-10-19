9032787dcd2d3b81da825e00441e5aa112303f13
andyscoffee <monarch071@gmail.com>
2022-10-19 13:09:54 +0900

    BOJ 1806

--- 백준/투 포인터/부분합.py
+++ 백준/투 포인터/부분합.py
@@ -4,9 +4,16 @@ import sys
 
 n, s = map(int, sys.stdin.readline().split())
 data = list(map(int, sys.stdin.readline().split()))
-start, end = 0, 1
-temp = data[0]+data[end]
-while start < end:
-    tmp = data[start]+data[end]
-    if tmp > s:
-        print()
+end, tmp = 0, 0
+ans = n+1  # 길이의 최대값
+for start in range(n):
+    while tmp < s and end < n:
+        tmp += data[end]
+        end += 1
+    if tmp >= s:
+        ans = min(ans, (end-start))  # 가장 짧은 길이 선택
+    tmp -= data[start]
+if ans == (n+1):
+    print(0)
+else:
+    print(ans)
