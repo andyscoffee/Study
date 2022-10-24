93084e4a96a8e582c66688bc4e8818fdc6ee050f
andyscoffee <monarch071@gmail.com>
2022-10-24 23:01:13 +0900

    COS pro 기출 1차 5번

---
+++ COS pro/1급/1-5.py
@@ -0,0 +1,26 @@
+# You may use import as below.
+#import math
+
+def solution(n):
+    # Write code here.
+    dp = [0]*(n+1)
+    dp[1] = 1
+    dp[2] = 4  # 1+3
+    for i in range(3, n+1):
+        dp[n] = 1 + (2*n-1) + dp[n-2] + 4*(n-1)*(n-2)
+# 1 + 2*n-1(우측 대각) + 2단계 이전 대각의 합 + 대각의 합에서 늘어난 수(같은 패턴에서 4*(n-1)씩 늘어난 수를 대각선(n-2)개 더 추가)
+    return dp[n]
+
+
+# The following is code to output testcase.
+n1 = 3
+ret1 = solution(n1)
+
+# Press Run button to receive output.
+print("Solution: return value of the function is", ret1, ".")
+
+n2 = 2
+ret2 = solution(n2)
+
+# Press Run button to receive output.
+print("Solution: return value of the function is", ret2, ".")
